80732b99f9957fb91026064e8cd0b50b283cbfcf
abdulmoiz7080-design <abdulmoiz7080@gmail.com>
2026-08-03 17:35:52 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function () {
+
+    setTimeout(function () {
+
+        document.querySelector(".loader").style.display = "none";
+
+    }, 2500);
+
+});
+
+document.querySelector("button").addEventListener("click", function () {
+
+    alert("A beautiful journey begins here 🤍");
+
+});
